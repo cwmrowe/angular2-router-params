1572e5841e98c8c3c05ea6d5ea52c3250690e8e8
Chris Rowe <chris@webcoda.co.uk>
2016-01-25 22:54:51 +0000

    example

---
+++ app/things.component.ts
@@ -0,0 +1,35 @@
+import {Component} from 'angular2/core';
+import {ROUTER_DIRECTIVES, RouteConfig, ComponentInstruction} from 'angular2/router';
+import {ThingsDetailComponent} from './things-detail.component';
+
+var markerCount = 0;
+
+@Component({
+    selector: 'empty-component',
+    template: "<p>EMPTY!!!</p>"
+})
+class EmptyComponent{}
+
+@Component({
+    selector: 'my-things',
+    template: `
+    <h2>My Things</h2>
+    <p>Search Term: {{searchTerm}}</p>
+    <router-outlet></router-outlet>`,
+    directives: [ROUTER_DIRECTIVES]
+})
+@RouteConfig([
+    {path: '/', useAsDefault: true, name: 'Empty', component: EmptyComponent},
+    {path: '/thing/:id', name: 'ThingsDetail', component: ThingsDetailComponent}
+])
+export class ThingsComponent { 
+    
+    searchTerm;
+    
+    routerOnActivate(next: ComponentInstruction, prev: ComponentInstruction){
+        if(next.params['search']){
+            this.searchTerm = next.params['search'];
+        }
+    }
+    
+}
